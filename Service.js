f090a2d4e7321ad5458e2b90c7d7048a5cece40e
dishamakkigadde2003 <122141995+dishamakkigadde2003@users.noreply.github.com>
2025-03-14 13:23:26 +0530

    Add files via upload

---
+++ Service.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Service() {
+  return (
+    <div>
+      <h2>Our Banking Services</h2>
+      <ul>
+        <li>Account Opening</li>
+        <li>Loans</li>
+        <li>Investments</li>
+        <li>Credit Cards</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Service;
